960d268fb37fa3539cc88bdf105d59a67d6787d7
jembrown <jeremymbrown@gmail.com>
2018-12-09 10:50:15 -0600

    Comments added for in-class assignment

--- week8_InClass.sh
+++ week8_InClass.sh
@@ -3,6 +3,7 @@
 #get user to input DNA sequence and print the complement of the sequence.
 complement = []
 dnaSeq = input ("Please enter your DNA sequence in all capitol letters: ")
+dnaSeq = dnaSeq.upper()
 
 #reverses DNA sequence that user inputs.
 dnaSeq = dnaSeq[::-1]
@@ -15,13 +16,16 @@ for i in dnaSeq:
 	elif i == "C":
 		complement.append("G")
 	elif i == "T":
-                complement.append("A")
+        complement.append("A")
 	elif i == "G":
-                complement.append("C")
+        complement.append("C")
 
 #converts list to a string
 str2 = ''.join(complement)
-print (str2)
+print (str2)	# DB: Minor, but better to avoid generic variable names like 'str2'. Maybe
+                #     try something like 'revComp'?
 
 
-
+# DB: Good! You do specify that the input sequence should be uppercase, but if you want
+#     to make sure, you could always use .upper() to convert whatever sequence the user
+#     provides.
